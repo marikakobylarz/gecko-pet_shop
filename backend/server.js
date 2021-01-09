338999885e5468f15fb729f4d555090311a7fb5a
Marika Kobylarz <akiramoo7@gmail.com>
2021-01-10 00:11:39 +0100

    Add and connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,41 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import mongoose from "mongoose";
+import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose.connect(
+  // eslint-disable-next-line no-undef
+  process.env.MONGODB_URL || "mongodb://localhost/gecko-pet_shop",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
-app.get('/api/products/:id', (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: 'Nie znaleziono produktu' });
+    res.status(404).send({ message: "Nie znaleziono produktu" });
   }
 });
 
-
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
-import logo from "./image/logo.jpg";
+import logo from "./image/logook-rev.jpg";
 import { useSelector } from "react-redux";
 
 function App() {
@@ -12,29 +12,33 @@ function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
-        <header className="row">
-          <div className="logobox row">
-            <img className="logo" src={logo} alt="logo" />
-            <Link to="/">
-              <h1 className="brand">gecko shop</h1>
-              <p className="brand">sklep zoologiczny</p>
-            </Link>
+        <header>
+          <div className="header_brand row">
+            <div className="logobox row">
+              <img className="logo" src={logo} alt="logo" />
+              <Link to="/">
+                <h1 className="brand">gecko shop</h1>
+                <p className="brand">sklep zoologiczny</p>
+              </Link>
+            </div>
+            <div>
+              <Link to="/cart">
+                Koszyk
+                {cartItems.length > 0 && (
+                  <span className="badge">{cartItems.length}</span>
+                )}
+              </Link>
+              <Link to="/signin">Zaloguj</Link>
+            </div>
           </div>
-          <div>
-            <Link to="/cart">
-              Koszyk
-              {cartItems.length > 0 && (
-                <span className="badge">{cartItems.length}</span>
-              )}
-            </Link>
-            <Link to="/signin">Zaloguj</Link>
-          </div>
-          {/* <nav className="row">
-            <ul>
+          <nav className="row">
+            <ul className="row">
               <li>Psy</li>
               <li>Koty</li>
+              <li>Gryzonie</li>
+              <li>Ptaki</li>
             </ul>
-          </nav> */}
+          </nav>
 
           {/* https://unsplash.com/photos/OOE4xAnBhKo?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink */}
         </header>
